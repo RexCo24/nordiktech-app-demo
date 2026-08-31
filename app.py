82f366c9b1fced24bbf69d6920e1123918a961eb
RexCo24 <koch-a@hotmail.com>
2026-08-31 08:08:13 +0000

    Add Prometheus-Flask-Exporter for Metrics Endpoint

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
 
 # --- Simple In‑Memory Inventory (später DB ersetzen) ---
 inventory = []
